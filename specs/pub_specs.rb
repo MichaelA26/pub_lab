89d8f35f83d7fce75287e79d0a7559609f64b566
Allan Milne <amilne.wd@gmail.com>
2019-08-07 11:38:52 +0100

    Pub_specs setup

---
+++ specs/pub_specs.rb
@@ -0,0 +1,32 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../pub')
+
+class PubTest < Minitest::Test
+
+  def setup
+    @pub = Pub.new("Dragons Den", 1000.00, [])
+  end
+
+    def test_pub_name
+      assert_equal("Dragons Den", @pub.pub_name)
+    end
+
+    def test_till_balance
+      assert_equal(1000.00, @pub.till_balance)
+    end
+
+    def test_drinks_menu
+      assert_equal(0, @pub.drinks_menu.length)
+    end
+
+
+
+
+
+
+
+
+
+
+end
